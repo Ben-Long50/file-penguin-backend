39ca8e195a90f8479548055f899ec244304d7bf8
Ben-Long50 <benjlong50@gmail.com>
2024-12-10 09:13:58 -0800

    added ping endpoint for server wakeup

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,6 +3,10 @@ import userController from '../controllers/userController.js';
 
 const router = express.Router();
 
+router.get('/status', (req, res) =>
+  res.status(200).json({ message: 'Service is running' }),
+);
+
 router.post('/auth/signup', userController.createUser);
 
 router.post('/auth/signin', userController.signinUser);
